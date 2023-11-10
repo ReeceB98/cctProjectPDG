46b624039bc8480f7514da7622c519bcb0c3196f
Reece <reecebellchambers98@gmail.com>
2023-11-10 20:13:01 +0000

    Instantiated the starting normal room prefab to the middle of the room grid

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -18,22 +18,47 @@ public class RoomManager : MonoBehaviour
     [Space]
     [Header("Other Settings")]
     [SerializeField] private List<GameObject> roomObjects = new List<GameObject>();
+    [SerializeField] private Queue<Vector2Int> roomQueue = new Queue<Vector2Int>();
     [SerializeField] private int[,] roomGrid;
-    [SerializeField] private int roomCount;
+    [SerializeField] private int roomCount = 0;
 
     private void Start()
     {
+        // Setting the size of the grid for the rooms
         roomGrid = new int[gridSizeX, gridSizeX];
+
+        // Putting rooms in a queue for the rooms in FIFO order
+        roomQueue = new Queue<Vector2Int>();
+
+        // The starting room will be spawned in the middle of the grid
+        Vector2Int initalRoomIndex = new Vector2Int(gridSizeX / 2, gridSizeY / 2);
+        StartRoomGenerationFromRoom(initalRoomIndex);
+    }
+
+    private void StartRoomGenerationFromRoom(Vector2Int roomIndex)
+    {
+        // Add the starting room to the queue
+        roomQueue.Enqueue(roomIndex);
+        int x = roomIndex.x;
+        int y = roomIndex.y;
+
+        // Instantiating the starting room prefab to the middle of the grid
+        roomGrid[x, y] = 1;     // storing the value to the middle of the grid array
+        roomCount++;
+        GameObject initialRoom = Instantiate(normalRoomPrefab, GetPositionFromGridIndex(roomIndex), Quaternion.identity);
+        initialRoom.name = $"Room - {roomCount}";
+        initialRoom.GetComponent<Room>().RoomIndex = roomIndex;
+        roomObjects.Add(initialRoom);
     }
 
     // Calculating individual grid positions in the grid index
-    private Vector2Int GetPositionFromGridIndex(Vector2Int gridIndex)
+    private Vector3 GetPositionFromGridIndex(Vector2Int gridIndex)
     {
         int gridX = gridIndex.x;
         int gridY = gridIndex.y;
 
         // returns the grid size for each point of the index for the rooms
-        return new Vector2Int(roomWidth * (gridX - gridSizeX / 2),
+        return new Vector3(roomWidth * (gridX - gridSizeX / 2),
             roomHeight * (gridY - gridSizeY / 2));
     }
 
@@ -47,8 +72,8 @@ public class RoomManager : MonoBehaviour
             for (int y = 0; y < gridSizeY; y++)
             {
                 // Getting the postion for each cell in the grid and drawing it to screen
-                Vector2Int position = GetPositionFromGridIndex(new Vector2Int(x, y));
-                Gizmos.DrawWireCube(new Vector3(position.x, position.y), new Vector3(roomWidth, roomHeight, 1.0f));
+                Vector3 position = GetPositionFromGridIndex(new Vector2Int(x, y));
+                Gizmos.DrawWireCube(position, new Vector3(roomWidth, roomHeight, 1.0f));
             }
         }
     }
